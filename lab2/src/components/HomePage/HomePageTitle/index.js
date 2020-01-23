55a5702277596bbadf16ef95327f7ebb4981933b
Vickee Johnson <vi436649@ucf.edu>
2020-01-23 11:45:04 -0500

    Build Code Added

--- lab2/src/components/HomePage/HomePageTitle/index.js
+++ lab2/src/components/HomePage/HomePageTitle/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./index.css";
 
 class HomePageTitle extends React.Component {
     render() {
       return (
-        <h1>Lab 2</h1>
+        <h1 className="homePageTitle">Lab 2: React Rendering</h1>
       );
     }
   }
