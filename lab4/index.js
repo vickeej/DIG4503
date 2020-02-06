7dc360093ef734ed1588f433472c91b03cfa7bcb
Vickee Johnson <vi436649@ucf.edu>
2020-02-06 11:33:39 -0500

    lab 4 in progress

--- lab4/index.js
+++ lab4/index.js
@@ -2,31 +2,19 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 const chalk = require('chalk');
-const Person = require('./Person.js');
 
-let people = [];
+const Pokemonz = require('./Pokemonz.js');
+let pokemonz = new Pokemonz();
 
-for(let i =0; i <87; i++) {
-    people.push();
-}
-
-App.get("/id/#id", (Request, Response) => {
-    for(let i =0; i <people.length; i++) {
-        if(request.params.name == people[i].id) {
-            Response.send(people[i].color);
-            console.log(chalk.green('Valid!'));
-        }
-    }
-    Request.send("Invalid!");
-    console.log(chalk.red('Invalid!'));
+App.get("/pokemons/name/#name", (req, res) => {
+    res.send(pokemonz.readName(req));
 });
 
-App.get("/name/#name", (Request, Response) => {
-    Response.send("Hello World!");
-    console.log(chalk.green('Valid!'));
-    console.log(chalk.red('Invalid!'));
+App.get("/pokemons/id/#id", (req, res) => {
+    res.send(pokemonz.readID(req));
 });
+
 
 App.listen(port, () => {
     console.log("Server running!");
-  });
+});
