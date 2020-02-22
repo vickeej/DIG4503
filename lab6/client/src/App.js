1790b2fa615a8b4384c62e8cb472d63743acf49c
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-02-22 11:33:29 -0500

    Lab 6 Part 2 Setup

---
+++ lab6/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <IdSearch />
+      <NameSearch />
+      <h2>Reporting</h2>
+      <div id="reportingArea"></div>
+    </div>
+  );
+}
+
+export default App;
