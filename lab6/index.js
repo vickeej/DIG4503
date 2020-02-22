24a6b6c0c5f1613e1408a06fe710c8a27cee1f86
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-02-22 11:27:32 -0500

    Completed Lab 6 Part 1

---
+++ lab6/index.js
@@ -0,0 +1,47 @@
+const Express = require("express");
+const App = Express();
+const Pokemon = require("json-pokemon");
+const chalk = require("chalk");
+const port = 80;
+
+const cors = require("cors");
+App.use(cors());
+
+App.get("/", Express.static("client/build"));
+
+App.get("/id/:id/", (req, res) => {
+
+    let result = "Invalid! ID does not exisit."; 
+
+    Pokemon.forEach((value) => {
+        if(value.id == req.params.id) {
+            result = value;
+            console.log(chalk.green(req.path));
+        }
+    });
+        
+        if(result == "Invalid! ID does not exisit.") {
+            console.log(chalk.red(req.path));
+        }
+        res.send(result);
+});
+
+App.get("/name/:name", (req, res) => {
+
+let result = "Invalid! Name does not exisit.";
+
+    Pokemon.forEach((value) => {
+        if(value.name == req.params.name) {
+            result = value;
+            console.log(chalk.green(req.path));
+        }
+    });
+    if(result == "Invalid! Name does not exisit.") {
+        console.log(chalk.red(req.path));
+    }
+    res.send(result);
+});
+
+App.listen(port, () => {
+    console.log("Server Running!");
+})
