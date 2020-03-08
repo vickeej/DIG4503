b6c2e457dac46614b6c71d182fdf3141f562c044
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-03-07 23:51:25 -0500

    Lab 7 Finished

--- lab7/client/src/index.js
+++ lab7/client/src/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const Express = require('express');
+const App = Express();
+const port = 80;
+const cors = require('cors');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+App.use(cors());
+
+const Database = require("./Database.js");
+
+let database = new Database();
+
+App.get("/movies/title/:title", (req, res) => {
+
+    let result = {"error": "Could not a movie with that title!"};
+
+    database.findTitle(req.params.title).then((movie) => {
+        if(movie != null) {
+            result = movie;
+        }
+        res.json(result);
+    });
+
+});
+
+App.get("/movies/year/:year", (req, res) => {
+    let result = {"error": "Could not find a movie in that year!"};
+
+    database.findYear(req.params.year).then((year) =>{
+        if(year != null){
+            result = year;
+        }
+        res.json(result);
+    });
+});
+
+App.listen(port, () => {
+    console.log("Server running")
+});
